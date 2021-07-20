88c6a1fc5659653dd635a5e3c2931f3142027990
Patryk Gołębiowski <Patryk.Golebiowski@qvc.com>
2021-07-21 00:38:50 +0200

    Created simple model for NonPrimaryADAccount

---
+++ powershell_web_app/NonPrimaryADAccounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class NonPrimaryADAccount(models.model):
+    PrimaryID = models.CharField(max_length=30)
+    SecondaryID = models.CharField(max_length=30)
+    FirstName = models.CharField(max_length=200)
+    Surname = models.CharField(max_length=200)
+    EmailAddress = models.EmailField(max_length=200)
+    JobTitle = models.CharField(max_length=200)
+    Department = models.CharField(max_length=200)
+    Company = models.CharField(max_length=200)
+    Manager = models.CharField(max_length=200)
+    ManagerID = models.CharField(max_length=30)
+    CreatedAt = models.DateField()
